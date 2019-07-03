a4cd4e5a707215d4efa529567315e2d44e7dfda9
Steve Dignam <steve@dignam.xyz>
2019-07-02 22:47:20 -0400

    bump version

--- flake8_pie.py
+++ flake8_pie.py
@@ -224,7 +224,7 @@ def is_celery_apply_async_missing_expires(node: ast.Call) -> Optional[ErrorLoc]:
 
 class Flake8PieCheck:
     name = "flake8-pie"
-    version = "0.3.0"
+    version = "0.4.0"
 
     def __init__(self, tree: ast.Module) -> None:
         self.tree = tree
