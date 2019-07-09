2e0ffdb5e09acf985cc9477ad85c7ce60dcf32f0
Steve Dignam <sbdchd@users.noreply.github.com>
2019-07-08 21:33:33 -0400

    fix: checking of name based decorators (#22)
    Previously @shared_task or @app.task weren't checked. Only called
    decorators like @shared_task() were.

--- flake8_pie.py
+++ flake8_pie.py
@@ -109,7 +109,11 @@ def is_assign_and_return(func: ast.FunctionDef) -> Optional[ErrorLoc]:
 
 
 def has_name_kwarg(dec: ast.Call) -> bool:
-    return all(k.arg != "name" for k in dec.keywords)
+    return any(k.arg == "name" for k in dec.keywords)
+
+
+CELERY_SHARED_TASK_NAME = "shared_task"
+CELERY_TASK_NAME = "task"
 
 
 def is_celery_task_missing_name(func: ast.FunctionDef) -> Optional[ErrorLoc]:
@@ -118,15 +122,27 @@ def is_celery_task_missing_name(func: ast.FunctionDef) -> Optional[ErrorLoc]:
     """
     if func.decorator_list:
         for dec in func.decorator_list:
+            if (
+                isinstance(dec, ast.Attribute)
+                and isinstance(dec.value, ast.Name)
+                and dec.attr == CELERY_TASK_NAME
+            ):
+                return PIE783(lineno=dec.lineno, col_offset=dec.col_offset)
+            if isinstance(dec, ast.Name) and dec.id == CELERY_SHARED_TASK_NAME:
+                return PIE783(lineno=dec.lineno, col_offset=dec.col_offset)
             if isinstance(dec, ast.Call):
-                if isinstance(dec.func, ast.Name):
-                    if dec.func.id == "shared_task":
-                        if has_name_kwarg(dec):
-                            return PIE783(lineno=dec.lineno, col_offset=dec.col_offset)
-                if isinstance(dec.func, ast.Attribute):
-                    if dec.func.attr == "task":
-                        if has_name_kwarg(dec):
-                            return PIE783(lineno=dec.lineno, col_offset=dec.col_offset)
+                if (
+                    isinstance(dec.func, ast.Name)
+                    and dec.func.id == CELERY_SHARED_TASK_NAME
+                    and not has_name_kwarg(dec)
+                ):
+                    return PIE783(lineno=dec.lineno, col_offset=dec.col_offset)
+                if (
+                    isinstance(dec.func, ast.Attribute)
+                    and dec.func.attr == CELERY_TASK_NAME
+                    and not has_name_kwarg(dec)
+                ):
+                    return PIE783(lineno=dec.lineno, col_offset=dec.col_offset)
     return None
 
 
